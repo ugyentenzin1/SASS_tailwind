0156bfa4353eec01f506b420b330406b0032754e
Ugyen Tenzin <ugyen.tenzin@selise.ch>
2023-09-22 16:02:30 +0600

    profile mobile view

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -9,7 +9,19 @@ export class HeaderComponent implements OnInit {
 
   constructor() { }
 
+  sideNav:boolean = false;
+  hasScrolled = false;
+
+
+  @HostListener("document:scroll")
+  navBgChange(){
+    document.body.scrollTop > 0 || document.documentElement.scrollTop > 0 ? this.hasScrolled = true : this.hasScrolled = false;
+  }
   ngOnInit(): void {
+  }
+
+  toogleSideNav() {
+    this.sideNav = !this.sideNav;
   }
 
 }
